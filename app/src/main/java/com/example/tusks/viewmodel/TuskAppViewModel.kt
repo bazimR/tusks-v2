6c078e23ea2d10c5cc8d1d42a0a292f1328f1b3a
rishal-ERPGULF <rishal@htsqatar.com>
2025-08-04 18:27:33 +0530

    added viewmodel and model

--- app/src/main/java/com/example/tusks/viewmodel/TuskAppViewModel.kt
+++ app/src/main/java/com/example/tusks/viewmodel/TuskAppViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.tusks.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.tusks.model.TodoData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
-class TuskAppViewModel: ViewModel() {
+class TuskAppViewModel : ViewModel() {
+    private val _todos = MutableStateFlow<List<TodoData>>(emptyList())
+    val todos = _todos.asStateFlow()
+
+    fun addTodo(todo: TodoData) {
+        _todos.update {
+            it + todo
+        }
+    }
 }
